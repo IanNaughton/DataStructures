2beea656f068642bafc6eab4ad03d28923c241d3
isnaught <iansnaughton@gmail.com>
2017-05-14 16:12:33 -0400

    First swing at creating a stack from a singly linked list. Next step--refactor the pop method. Recursion would be cleaner looking, but I hav already used recursion for traversing linked lists in this solution already. Constraints make us flex thos developer muscles :)

--- LinkedList/Stack/StackItem.cs
+++ LinkedList/Stack/StackItem.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace LinkedList.Stack
 {
-    public class StackItem
+    public class StackItem<T>
     {
+        public StackItem(T newValue)
+        {
+            Value = newValue;
+        }
+
+        public StackItem<T> Next { get; set; }
+
+        public T Value { get; set; }
     }
 }
